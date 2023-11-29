3c745422b84fa9680438ebc924a8af7253e0fec6
Nik Pietanze <nikpietanze@gmail.com>
2023-11-29 10:50:08 -0600

    remove web from api

--- main.go
+++ main.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
-	"html/template"
-	"io"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -18,19 +14,6 @@ import (
 	"nicked.io/scraper"
 )
 
-type TemplateRegistry struct {
-	templates map[string]*template.Template
-}
-
-func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	tmpl, ok := t.templates[name]
-	if !ok {
-		err := errors.New("Template not found: " + name)
-		return err
-	}
-	return tmpl.ExecuteTemplate(w, "base.html", data)
-}
-
 func main() {
 	e := echo.New()
 
@@ -42,49 +25,9 @@ func main() {
 		panic(err)
 	}
 
-	// Templates
-	templates := make(map[string]*template.Template)
-	templates["home.html"] = template.Must(template.ParseFiles("views/home.html", "views/layouts/base.html"))
-	templates["contact.html"] = template.Must(template.ParseFiles("views/contact.html", "views/layouts/base.html"))
-	templates["privacy.html"] = template.Must(template.ParseFiles("views/privacy.html", "views/layouts/base.html"))
-	templates["info/sales-and-discounts-tracking/v1.html"] = template.Must(template.ParseFiles("views/info/sales-and-discounts-tracking/v1.html", "views/layouts/base.html"))
-
-	e.Renderer = &TemplateRegistry{
-		templates: templates,
-	}
-
-	// Static Files
-	e.Static("/static", "public")
-
 	// Global Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
-
-	// Website Routes
-	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "home.html", map[string]interface{}{
-			"title": "Nicked",
-		})
-	})
-	e.GET("/contact", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "contact.html", map[string]interface{}{
-			"title": "Contact Us | Nicked",
-		})
-	})
-	e.GET("/privacy", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "privacy.html", map[string]interface{}{
-			"title": "Privacy Policy | Nicked",
-		})
-	})
-
-	// Info Routes
-	info := e.Group("/info")
-	info.GET("/sales-and-discounts-tracking", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "info/sales-and-discounts-tracking/v1.html", map[string]interface{}{
-			"title":       "Save More, Shop Smarter: The Power of a Sales and Discount Tracker!",
-			"description": "Discover the Ultimate Sales and Discount Tracker for Amazon Shoppers! Maximize Savings, Stay Organized, and Never Miss a Deal. Your Must-Have Companion for a Smarter Amazon Shopping Experience. Start Supercharging Your Savings Today!",
-		})
-	})
 
 	// API Routes
 	api := e.Group("/api")
@@ -117,8 +60,6 @@ func main() {
 	go scraper.Init()
 
 	e.Logger.Fatal(e.Start(":8080"))
-
-
 }
 
 func createSchema(ctx context.Context) error {
